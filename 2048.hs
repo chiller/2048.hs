65f9757dee4277bd2344f9f796f11508c84e87e2
Galaczi Endre <galacziendre@gmail.com>
2015-02-19 12:23:39 +0100

    better interactivity

--- 2048.hs
+++ 2048.hs
@@ -1,4 +1,5 @@
-import Data.Char 
+import System.Console.Haskeline
+
 merge (x:y:xs) 
     | x == y = [x+x, 0] ++ merge(xs)
     | otherwise = [x] ++ merge(y:xs)
@@ -9,21 +10,16 @@ shift xs = filter (\x -> x /= 0) xs ++ filter (\x -> x == 0) xs
 mergeleft = shift . merge . shift
 mergeright = reverse . mergeleft . reverse
 
+printTbl  = unwords . map show 
 
-readinput tbl = do
-    print tbl
-    x <- getChar
-    if x == 'x' then
-        return []
-    else if x == 'l' then
-        readinput (mergeleft tbl)
-    else if x == 'r' then
-        readinput (mergeright tbl)
-    else
-        readinput tbl
-
-
-main = do
-    readinput [32,0,32,0] 
-    print "done"
-
+main = runInputT defaultSettings (loop [32,0,32,0])
+    where 
+        loop chr = do
+            minput <- getInputChar $ printTbl chr
+            case minput of
+                Nothing -> return ()
+                Just 'q' -> return ()
+                Just 'l' -> loop $ mergeleft chr
+                Just 'r' -> loop $ mergeright chr
+                Just input -> do outputStrLn $ "Input was: " ++ [input]
+                                 loop chr

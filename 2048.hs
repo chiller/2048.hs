ffd0b08812a081082cfbe956e84da35a939912a2
Galaczi Endre <galacziendre@gmail.com>
2015-02-19 15:59:50 +0100

    even better scan

--- 2048.hs
+++ 2048.hs
@@ -1,4 +1,5 @@
-import System.Console.Haskeline
+import System.IO
+import Data.Char
 
 merge (x:y:xs) 
     | x == y = [x+x, 0] ++ merge(xs)
@@ -10,16 +11,21 @@ shift xs = filter (\x -> x /= 0) xs ++ filter (\x -> x == 0) xs
 mergeleft = shift . merge . shift
 mergeright = reverse . mergeleft . reverse
 
+printTbl :: [Int] -> String
 printTbl  = unwords . map show 
 
-main = runInputT defaultSettings (loop [32,0,32,0])
-    where 
-        loop chr = do
-            minput <- getInputChar $ printTbl chr
-            case minput of
-                Nothing -> return ()
-                Just 'q' -> return ()
-                Just 'l' -> loop $ mergeleft chr
-                Just 'r' -> loop $ mergeright chr
-                Just input -> do outputStrLn $ "Input was: " ++ [input]
-                                 loop chr
+
+main = do
+  hSetEcho stdin False
+  hSetBuffering stdin NoBuffering
+  hSetBuffering stdout NoBuffering
+  loop [32,0,32,0]
+      where loop tbl = do 
+              putStrLn $ "\n" ++ printTbl tbl ++ "\n"
+              c <- hGetChar stdin
+              case c of
+                'q' -> return ()
+                'a' -> loop $ mergeleft tbl
+                'd' -> loop $ mergeright tbl
+                input -> loop tbl
+
